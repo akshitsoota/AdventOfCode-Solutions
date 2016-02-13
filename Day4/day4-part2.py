3f0e316f3603649e84bb9950ddc6cf1615a4924c
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2016-02-13 16:02:51 -0500

    Added Day 4 (Part 1 and 2) code

---
+++ Day4/day4-part2.py
@@ -0,0 +1,17 @@
+import hashlib
+
+# Base input
+base_input = "yzbqklnj"
+# Start looping over and check
+number = 1
+while True:
+    find_hash_of = base_input + str(number)
+    # Solve for the MD5 hash now
+    md5_hash = hashlib.md5(find_hash_of).hexdigest()
+    # Now check if we can find the 5 zeros or not
+    if md5_hash[:6] == "000000":
+        break
+    # Increment the number
+    number += 1
+# Print the input string that works now
+print number
